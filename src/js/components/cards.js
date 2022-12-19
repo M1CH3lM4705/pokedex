96b61ec13bd303626c61d7ede6802ff0974af5c4
Michel Matos <michel.mef@gmail.com>
2022-12-19 19:30:19 -0400

    Add loader gif in the web

--- src/js/components/cards.js
+++ src/js/components/cards.js
@@ -1,9 +1,10 @@
 import card from "./card-pokemon.js"
 
+const loader = 'https://weichiachang.github.io/pokemon-master/img/loading.45600eb9.gif'
 const cards = () => {
-
     const insertPokemonsInDom = async ({ getPokemons }) => {
         const cards = document.querySelector('[data-js="cards"]')
+        cards.innerHTML = `<img src="${loader}" />` 
         const pokemons = await getPokemons()
         if(!pokemons) return
         
