e427ebb40cda12cf1e1f9fbe7caca20a716e5f77
Michel Matos <michel.mef@gmail.com>
2023-04-26 17:09:00 -0400

    lazy-loading

--- src/js/components/card-pokemon.js
+++ src/js/components/card-pokemon.js
@@ -1,7 +1,7 @@
 const card = (name, image, types) => 
     `<div class="card">
           <h2 class="card-title capitalized">${name}</h2>
-          <img src="${image}" alt="${name}">
+          <img src="${image}" loading="lazy" alt="${name}">
           <p class="card-desc">${types}</p>
     </div>`
     
